e03b9d0e975409f552a70d8a8ded5e353d0ff344
Conrad Lin <git@conradlin.com>
2023-05-30 16:37:26 +0800

    re-ordering check boxes

--- pages/pages/r2n.js
+++ pages/pages/r2n.js
@@ -318,10 +318,6 @@ const R2N = () => {
                     <Form.Check.Input type="checkbox" name="summary" onChange={(e) => setSummary(e.target.checked)} checked={summary} disabled />
                     <Form.Check.Label>Summary</Form.Check.Label>
                   </Form.Check>
-                  <Form.Check id="sentiment">
-                    <Form.Check.Input type="checkbox" name="sentiment" onChange={(e) => setSentiment(e.target.checked)} checked={sentiment} />
-                    <Form.Check.Label>Sentiment</Form.Check.Label>
-                  </Form.Check>
                   <Form.Check id="main_points">
                     <Form.Check.Input type="checkbox" name="main_points" onChange={(e) => setMain_Points(e.target.checked)} checked={main_points} />
                     <Form.Check.Label>Main Points</Form.Check.Label>
@@ -330,17 +326,21 @@ const R2N = () => {
                     <Form.Check.Input type="checkbox" name="action_items" onChange={(e) => setAction_Items(e.target.checked)} checked={action_items} />
                     <Form.Check.Label>Action Items</Form.Check.Label>
                   </Form.Check>
-                  <Form.Check id="transcript_arguments">
-                    <Form.Check.Input type="checkbox" name="transcript_arguments" onChange={(e) => setTranscript_Arguments(e.target.checked)} checked={transcript_arguments} />
-                    <Form.Check.Label>Transcript Arguments</Form.Check.Label>
-                  </Form.Check>
                   <Form.Check id="follow_up">
                     <Form.Check.Input type="checkbox" name="follow_up" onChange={(e) => setFollow_Up(e.target.checked)} checked={follow_up} />
                     <Form.Check.Label>Follow Up</Form.Check.Label>
                   </Form.Check>
+                  <Form.Check id="transcript_arguments">
+                    <Form.Check.Input type="checkbox" name="transcript_arguments" onChange={(e) => setTranscript_Arguments(e.target.checked)} checked={transcript_arguments} />
+                    <Form.Check.Label>Transcript Arguments</Form.Check.Label>
+                  </Form.Check>
                   <Form.Check className="mb-2" id="related_topics">
                     <Form.Check.Input type="checkbox" name="related_topics" onChange={(e) => setRelated_Topics(e.target.checked)} checked={related_topics} />
                     <Form.Check.Label>Related Topics</Form.Check.Label>
+                  </Form.Check>
+                  <Form.Check id="sentiment">
+                    <Form.Check.Input type="checkbox" name="sentiment" onChange={(e) => setSentiment(e.target.checked)} checked={sentiment} />
+                    <Form.Check.Label>Sentiment</Form.Check.Label>
                   </Form.Check>
                   {/* <input type="text" className="form-control" placeholder="Specific Prompt" id="prompt" required  onChange={(e) => setCustom_Prompt(e.target.value)}/> */}
                 </Col>
